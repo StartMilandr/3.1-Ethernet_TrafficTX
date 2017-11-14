6d171d0d2ed4b096eaddcb98c69ce647b6a6118a
StartMilandr <Info@StartMilandr.ru>
2017-11-14 11:55:39 +0300

    Стартовый проект

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "Eth_TrafficTX.h"
+
+int main()
+{
+	//	Тактирование
+	Clock_Init();
+		
+	//  Инициализация и запуск
+	Ethernet_Init();
+  Ethernet_Start();
+	//	Бесконечный цикл
+  Ethernet_ProcessLoop();	
+}	
+
+
